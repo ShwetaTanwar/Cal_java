b55f631e827e8da53da49e29894e1df42a4e95e2
ShwetaTanwar <89072715+ShwetaTanwar@users.noreply.github.com>
2022-07-19 12:33:31 +0530

    Delete blog_app directory

--- blog_app/src/main/java/initializ/blog/repositories/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package initializ.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import initializ.blog.entities.Comment;
-
-public interface CommentRepo extends JpaRepository<Comment, Integer> {
-
-}
--- blog_app/src/main/java/initializ/blog/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package initializ.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import initializ.blog.entities.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
